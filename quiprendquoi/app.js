e9ec0c6e8956aecadfb868f1d4d9197a68397536
Lcunault <lucas.cunault@gmail.com>
2020-03-22 15:49:01 +0100

    fin parti 1

--- quiprendquoi/app.js
+++ quiprendquoi/app.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(bodyParser);
+méthode `GET`
+url : `/party/:id`
+console.log(req.params.id)
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
+axios
+  .post(`${process.env.API_URL}/party`, req.body)
+  .then(({ data }) => res.redirect(`/party/${data._id}`))
+  .catch((err) => res.send(err));
+
+axios
+  .get(`${process.env.API_URL}/party/${req.params.id}`)
+  .then(({ data }) =>
+    res.render('party', {
+      party: data,
+      title: party.name
+    }),
+  )
+  .catch((err) => console.log(err));
+
+  res.render('party', {
+    party: data,
+    title: data.name,
+    url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+  }), 
+  
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${port}!`));
 const dotenv = require('dotenv');
 app.set('view engine', 'pug');
+app.use(bodyParser);
+console.log(req.params);
+const axios = require('axios');
+
+
+
+
+
+
+
